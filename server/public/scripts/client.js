d0c7de4e8c66da0afa5862eab7e3133b0cbdc147
Padee Vang <vangpadee11@gmail.com>
2024-07-15 19:10:35 -0500

    submit button works. able to add data to table and its on the DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,7 +2,32 @@ console.log('hello js');
 
 // this button will submit the values from the input fields
 
+
+// let fName = document.querySelector('#fName-input').value;
+// let employeeData = document.querySelector('#employeeData');
+
+
+
 function submitForm(event){
     event.preventDefault();
     console.log('checking button');
+
+    let fName = document.querySelector('#fName-input').value;
+    let lName = document.querySelector('#lName-input').value;
+    let id = document.querySelector('#id-input').value;
+    let title = document.querySelector('#title-input').value;
+    let salary = document.querySelector('#aSalary-input').value;
+    let employeeData = document.querySelector('#employeeData');
+
+    //console.log('first name', fName);
+    employeeData.innerHTML += `
+    <tr>
+        <td>${fName}</td>
+        <td>${lName}</td>
+        <td>${id}</td>
+        <td>${title}</td>
+        <td>${salary}</td>
+    
+    </tr>
+    `;
 }
